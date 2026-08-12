ee1cace21fc1d6d6aabb7e9ed5f66bd4ecc8c1e3
apple75 <guoping_tan@hotmail.com>
2026-08-12 22:12:48 +0800

    清理调试过程中产生的临时 py 文件（保留 push_cli.py）

--- format_nb.py
+++
@@ -1,9 +0,0 @@
-﻿import os, nbformat
-
-base = r"C:\Users\guopi\GitHub\courses\future-media-internet\labs\kaggle-demos"
-for fname in os.listdir(base):
-    if fname.endswith(".ipynb"):
-        path = os.path.join(base, fname)
-        nb = nbformat.read(path, as_version=4)
-        nbformat.write(nb, path)
-        print(f"Formatted: {fname}")
